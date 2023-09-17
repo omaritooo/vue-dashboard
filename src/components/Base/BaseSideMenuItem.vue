22887e89547790efa3a20b2e6d9d6ba7cba5742f
omaritooo <themedic227@gmail.com>
2023-09-18 00:40:46 +0300

    Add sidebar

---
+++ src/components/Base/BaseSideMenuItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <RouterLink :to="route.route">
+    <component :is="componentIcon[route.name as keyof typeof componentIcon]" />
+    <span class="w-full">{{ route.name }}</span>
+  </RouterLink>
+</template>
+<script setup lang="ts">
+import type { Route } from "@/types";
+import { defineAsyncComponent } from "vue";
+import { RouterLink } from "vue-router";
+
+const IconDashboard = defineAsyncComponent(() => import("../Icon/IconDashboard.vue"));
+const IconLeaderboard = defineAsyncComponent(() => import("../Icon/IconLeaderboard.vue"));
+const IconProduct = defineAsyncComponent(() => import("../Icon/IconProduct.vue"));
+const IconOrder = defineAsyncComponent(() => import("../Icon/IconOrder.vue"));
+const IconMessages = defineAsyncComponent(() => import("../Icon/IconMessages.vue"));
+const IconSettings = defineAsyncComponent(() => import("../Icon/IconSettings.vue"));
+defineProps<{
+  route: Route;
+}>();
+
+const componentIcon = {
+  Dashboard: IconDashboard,
+  Leaderboard: IconLeaderboard,
+  Products: IconProduct,
+  Order: IconOrder,
+  Messages: IconMessages,
+  Settings: IconSettings,
+};
+</script>
+<style scoped>
+a {
+  color: black;
+  width: 100%;
+  min-width: 150px;
+  gap: 10px;
+  padding: 0 10px;
+  height: 45px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+a:hover {
+  background-color: #5d5fef;
+  color: white;
+}
+
+a.router-link-exact-active {
+  background-color: #5d5fef;
+  color: white;
+}
+/* a.router-link-active {
+  background-color: red;
+} */
+</style>
---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,61 @@
+<template>
+  <aside
+    class="fixed top-0 left-0 z-40 flex flex-col justify-between w-1/6 h-screen px-6 py-8 bg-white shadow-lg rounded-r-md"
+  >
+    <h1 class="flex items-center justify-start text-black gap-x-2">
+      <IconLogo />
+      <span class="text-xl"> Dashboard </span>
+    </h1>
+    <ul class="flex flex-col w-full mt-6 h-fit gap-y-3">
+      <BaseSideMenuItem v-for="route in routes" :key="route.name" :route="route" />
+    </ul>
+    <div
+      class="relative z-40 flex flex-col items-center w-full p-4 mt-auto rounded-lg bg-primary"
+    >
+      <div class="flex flex-col items-center">
+        <IconLogo />
+        <span class="text-base font-bold text-white"> Dashboard Pro </span>
+        <p class="w-3/4 text-sm text-center text-gray-300">
+          Get access to all features on tetumbas
+        </p>
+        <button
+          class="w-3/4 py-1 mt-4 text-base font-bold bg-white rounded-sm text-primary"
+        >
+          Get Pro
+        </button>
+      </div>
+    </div>
+  </aside>
+</template>
+<script setup lang="ts">
+import IconLogo from "./Icon/IconLogo.vue";
+import BaseSideMenuItem from "./Base/BaseSideMenuItem.vue";
+import { reactive } from "vue";
+import type { Route } from "@/types";
+const routes: Route[] = reactive([
+  {
+    name: "Dashboard",
+    route: "/",
+  },
+  {
+    name: "Leaderboard",
+    route: "/leaderboard",
+  },
+  {
+    name: "Order",
+    route: "/order",
+  },
+  {
+    name: "Products",
+    route: "/products",
+  },
+  {
+    name: "Messages",
+    route: "/messages",
+  },
+  {
+    name: "Settings",
+    route: "/settings",
+  },
+]);
+</script>
